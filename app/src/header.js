0fc22871b9e1fd5678aea1b495f9878c95f565c3
Danton philip Ruthe <845857108@bun2-206-19.local>
2018-09-12 11:52:35 +1200

    Finished header layout, started converting to react component

--- app/src/header.js
+++ app/src/header.js
@@ -8,7 +8,26 @@ class Header extends Component{
   render(){
     return (
       <div>
+        <div className="header">
+          <div class="header-top">
+            <form class="filter-form" action="">
+              <input class="filter-list" type="radio" name="gender" value="cocktails" checked /><img class="icon" src="icons/cocktail.png" alt="" /> Cocktail
+              <input class="filter-list" type="radio" name="gender" value="burgers" /><img class="icon" src="icons/hamburger-meal.png" alt="" />Burger
+              <input class="filter-list" type="radio" name="gender" value="dine" /><img class="icon" src="icons/cutlery.png" alt="" /> Dine
+            </form>
+          </div>
 
+          <div class="header-bottom">
+					<button class="btn purple-btn" value="protein">Protein</button>
+
+
+					<h3>Sort by</h3>
+					<select class="sort-by">
+						<option value="alphabetical">Alphabetical</option>
+						<option value="price">Price</option>
+					</select>
+          </div>
+        </div>
       </div>
     )
   }
