b48325b38bd8081a0f2e30e7696940cfa35a7f11
Danton philip Ruthe <845857108@bun2-206-19.local>
2018-09-12 11:20:44 +1200

    Creating components, and template for information

---
+++ app/src/header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Header extends Component{
+  constructor(props){
+
+  }
+
+  render(){
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default Header
