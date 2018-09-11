0b1e469a14d3d54bac6c73048ca89c553ccb288e
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2018-09-11 23:46:13 +0000

    started getting data from server

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,46 @@
-if (12 === 11) {
-  console.log('pass')
-} else {
-  console.log('fail')
+import React, { Component } from 'React'
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    }
+  }
+
+  componentDidMount () {
+    fetch('http://192.168.33.10:5000/burger')
+      .then(res => res.JSON())
+      .then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          })
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          })
+        }
+      )
+  }
+
+  render () {
+    const { error, isLoaded, items } = this.state
+    if (error) {
+      return <div>Error: {error.message}</div>
+    } else if (!isLoaded) {
+      return <div>Loading...</div>
+    } else {
+      return (
+        console.log(items)
+      )
+    }
+  }
 }
+
+export default App
