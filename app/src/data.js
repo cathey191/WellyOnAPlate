d7989ee91ed729704ba7a1050c3265e70b193e98
catherine <cathey191@hotmail.com>
2018-09-13 09:38:23 +1200

    React getting woap data from server

---
+++ app/src/data.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+
+class Woapdata extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    }
+  }
+
+  componentDidMount () {
+    fetch('http://192.168.33.10:5000/cocktail')
+      .then(res => res.json())
+      .then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          })
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          })
+        }
+      )
+  }
+
+  render () {
+    const { error, isLoaded, items } = this.state
+    if (error) {
+      return <div>Error: {error.message}</div>
+    } else if (!isLoaded) {
+      return <div>Loading...</div>
+    } else {
+      return (
+        <div>
+          {this.state.items.map((item) => <p>{item[0].company}</p>)}
+        </div>
+      )
+    }
+  }
+}
+
+export default Woapdata
--- app/src/index.js
+++ app/src/index.js
@@ -1,46 +1,6 @@
-import React, { Component } from 'React'
+import React from 'react'
+import ReactDom from 'react-dom'
 
-class App extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      error: null,
-      isLoaded: false,
-      items: []
-    }
-  }
+import Woapdata from './data.js'
 
-  componentDidMount () {
-    fetch('http://192.168.33.10:5000/burger')
-      .then(res => res.JSON())
-      .then(
-        result => {
-          this.setState({
-            isLoaded: true,
-            items: result
-          })
-        },
-        error => {
-          this.setState({
-            isLoaded: true,
-            error
-          })
-        }
-      )
-  }
-
-  render () {
-    const { error, isLoaded, items } = this.state
-    if (error) {
-      return <div>Error: {error.message}</div>
-    } else if (!isLoaded) {
-      return <div>Loading...</div>
-    } else {
-      return (
-        console.log(items)
-      )
-    }
-  }
-}
-
-export default App
+ReactDom.render(<Woapdata />, document.querySelector('#test'))
