68ce2b96b9bfdb65a9d649980f467950250acd65
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2018-09-13 04:46:12 +0000

    Converted list item template in HTML to React

--- app/src/index.js
+++ app/src/index.js
@@ -2,5 +2,7 @@ import React from 'react'
 import ReactDom from 'react-dom'
 
 import Woapdata from './data.js'
+import Items from './items.js'
 
-ReactDom.render(<Woapdata />, document.querySelector('#list'))
+// ReactDom.render(<Woapdata />, document.querySelector('#list'))
+ReactDom.render(<Items />, document.querySelector('#list'))
---
+++ app/src/items.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import ReactDom from 'react-dom'
+
+import Woapdata from './data.js'
+
+class Items extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <div className='list-item'>
+                    <h1 className='venue-name'>1815 Cafe &amp; Bar<span className='item-cost'>$22</span></h1>
+                    <h2 className='item-name'><img src='./icons/hamburger-meal.png' className='icon-small' />Buns got soul</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+
+                    <div className="list-bottom">
+                        <button className="btn btn-visit">Visit Site</button>
+                    </div>
+                </div>
+                <hr className='list-break' />
+
+                <div className='list-item'>
+                    <h1 className='venue-name'>Annam Restaurant<span className='item-cost'>$22</span></h1>
+                    <h2 className='item-name'><img src='./icons/cutlery.png' className='icon-small' />Festival Dish</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                    <h2 className='item-name'>Dessert</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+
+
+                    <div className="list-bottom">
+                        <button className="btn btn-visit">Visit Site</button>
+                    </div>
+                </div>
+                <hr className='list-break' />
+            </div>
+
+
+
+        )
+    }
+}
+
+export default Items;
