ff4a5ad5b7cd8c27da4fcb0b99003bbdb4308190
catherine <cathey191@hotmail.com>
2018-09-14 11:46:12 +1200

    results changing when different event option is chosen

--- app/src/data.js
+++ app/src/data.js
@@ -10,8 +10,27 @@ class Woapdata extends Component {
     }
   }
 
-  componentDidMount () {
-    fetch('http://localhost:5000/' + this.props.current)
+  componentWillMount () {
+    fetch('http://192.168.33.10:5000/burger')
+      .then(res => res.json())
+      .then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          })
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          })
+        }
+      )
+  }
+
+  componentWillReceiveProps (nextProps) {
+    fetch('http://192.168.33.10:5000/' + nextProps.current)
       .then(res => res.json())
       .then(
         result => {
@@ -40,7 +59,7 @@ class Woapdata extends Component {
         <div>
           {this.state.items.map((item, i) => <div className='list' key={i}>
             <h1 className='venue-name'>{item[0].company}<span className='item-cost'>$22</span></h1>
-            <h2 className='item-name'>Buns got soul</h2>
+            <h2 className='item-name'>{item[1][0].event}</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
           </div>
           )}
