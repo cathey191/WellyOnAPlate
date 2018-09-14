b9aec2ad55d97a56278040aab8a78012ab939d51
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2018-09-14 04:49:57 +0000

    Finished basic list component

--- app/src/items.js
+++ app/src/items.js
@@ -12,8 +12,6 @@ class Items extends Component {
             text: 'here is some text',
             items: this.props.allItems
         } // this.state end
-
-        this.testFunction = this.testFunction.bind(this)
     }
 
     render() {
@@ -23,12 +21,12 @@ class Items extends Component {
                     {this.props.allItems.map((item, i) =>
                         <div key={i}>
                             <div className='list-item'>
-                                <h1 className='venue-name'>{item["0"].company}<span className='item-cost'>${item["1"]["0"].price}</span></h1>
+                                <h1 className='venue-name'>{item[0].company}<span className='item-cost'>${item[1][0].price}</span></h1>
                                 <h2 className='item-name'><img src='./icons/hamburger-meal.png' className='icon-small' />{item[1][0].title}</h2>
-                                <p>{item[1]["0"].description}</p>
+                                <p>{item[1][0].description}</p>
 
-                                <div className="list-bottom">
-                                    <button className="btn btn-visit" onClick={this.testFunction}>Visit Site</button>
+                                <div className='list-bottom'>
+                                    <button className='btn btn-visit'>Visit Site</button>
                                 </div>
                             </div>
                             <hr className='list-break' />
@@ -43,9 +41,9 @@ class Items extends Component {
                        this.props.allItems.map(function (item, i) {
                            if (item[1][0].dishes.length === 3){
                                return  (
-                                   <div key={i}>
+                                <div key={i}>
                                    <div className='list-item'>
-                                       <h1 className='venue-name'>{item["0"].company}<span className='item-cost'>{item["1"]["0"].price}</span></h1>
+                                       <h1 className='venue-name'>{item[0].company}<span className='item-cost'>{item[1][0].price}</span></h1>
                                        <h2 className='item-name'><img src='./icons/cutlery.png' className='icon-small' />{item[1][0].dishes[0].course1_type}</h2>
                                        <p>{item[1][0].dishes[0].course1}</p>
                                        <h2 className='item-name'>{item[1][0].dishes[1].course2_type}</h2>
@@ -54,13 +52,31 @@ class Items extends Component {
                                        <p>{item[1][0].dishes[2].course3}</p>
        
        
-                                       <div className="list-bottom">
-                                           <button className="btn btn-visit">Visit Site</button>
+                                       <div className='list-bottom'>
+                                           <button className='btn btn-visit'>Visit Site</button>
                                        </div>
                                    </div>
                                    <hr className='list-break' />
                                </div>
                                )
+                        } else {
+                            return (
+                                <div key={i}>
+                                   <div className='list-item'>
+                                       <h1 className='venue-name'>{item[0].company}<span className='item-cost'>{item[1][0].price}</span></h1>
+                                       <h2 className='item-name'><img src='./icons/cutlery.png' className='icon-small' />{item[1][0].dishes[0].course1_type}</h2>
+                                       <p>{item[1][0].dishes[0].course1}</p>
+                                       <h2 className='item-name'>{item[1][0].dishes[1].course2_type}</h2>
+                                       <p>{item[1][0].dishes[1].course2}</p>
+       
+       
+                                       <div className='list-bottom'>
+                                           <button className='btn btn-visit'>Visit Site</button>
+                                       </div>
+                                   </div>
+                                   <hr className='list-break' />
+                               </div>
+                            )
                         }
                         
                                
@@ -69,37 +85,10 @@ class Items extends Component {
                     }
                </div>
             )
-
-                
-            // return (
-                // <div>
-                //     {this.props.allItems.map((item, i) =>
-                        // <div key={i}>
-                        //     <div className='list-item'>
-                        //         <h1 className='venue-name'>{item["0"].company}<span className='item-cost'>{item["1"]["0"].price}</span></h1>
-                        //         <h2 className='item-name'><img src='./icons/cutlery.png' className='icon-small' />{item[1][0].dishes[0].course1_type}</h2>
-                        //         <p>{item[1][0].dishes[0].course1}</p>
-                        //         <h2 className='item-name'>{item[1][0].dishes[1].course2_type}</h2>
-                        //         <p>{item[1][0].dishes[1].course2}</p>
-
-
-                        //         <div className="list-bottom">
-                        //             <button className="btn btn-visit">Visit Site</button>
-                        //         </div>
-                        //     </div>
-                        //     <hr className='list-break' />
-                        // </div>
-                //     )}
-                // </div>
-            // )
         }
 
     } // render ends
     
-
-    testFunction() {
-        console.log(this.state.items[0][1][0].event)
-    }
 
 }
 
