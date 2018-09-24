abef3afb96c7ab40f310088a2b3ca6423328a9be
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2018-09-24 06:21:47 +0000

    Creating markers based on data

--- app/src/mapdata.js
+++ app/src/mapdata.js
@@ -1,8 +1,7 @@
- import React, { Component } from 'react'
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
-import key from './config.json';
+import React, { Component } from 'react'
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react'
+import key from './config.json'
 import Geocode from 'react-geocode'
-import { log } from 'util';
 
 Geocode.setApiKey(key[0].API_KEY)
 // Geocode.enableDebug()
@@ -11,105 +10,92 @@ export class MapContainer extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      itemsFromProps : this.props.items,
+      itemsFromProps: this.props.items,
       locationArray: [],
-      text: 'text'
+      text: 'text',
+      isLoaded: false
     }
     this.getLocations = this.getLocations.bind(this)
   }
 
-  componentDidMount(){
+  componentDidMount () {
+    console.log(this.props.items.length);
     this.getLocations()
     
+  }
+  
+  componentDidUpdate(){
     
-    
-
   }
 
-  render() {
-    console.log(this.state.locationArray);
-    
-    return(
-      <div>
-        <Map 
-          google={this.props.google} 
-          zoom={14}
-          visible={true}
-          initialCenter={{
-            lat: -41.286461,
-            lng: 174.776230
-          }}
-          >
-          {/* {this.locationsArray.map((locations, i)=>
-            <Marker
-              key = {i}
-              name={'Name'}
-              position={locations}
-            />
-          )} */}
-        </Map>
-      </div>
-          )
-        }
-          
-        getLocations(){   
-          console.log('running');
-           
-          let locations = [];
-          for (let i = 0; i < 10; i++) {
-            var el = this.props.items[i];
-            
-            Geocode.fromAddress(el["0"].address1 + ' Wellington, New Zealand').then(
-              response => {
-                // console.log(response.results[0].geometry.location.lat);
-                // console.log(response.results[0].geometry.location.lng);
-                // var lat = response.results[0].geometry.location.lat
-                // var lng = response.results[0].geometry.location.lng
-                
-                var test = response.results[0].geometry.location
-                // console.log(this.state.locationArray);
-                
-                // console.log(test);
-                var joined = this.state.locationArray.concat({test})
-                // console.log(joined);
-                // locations.push(test)
-                if (i + 1 === 10){
-                  this.setState({
-                    locationArray: joined
-                  })
-                }
-              },
-              error => {
-                console.log(error);
-                
-              }
-                
-            )
-          }
-          
-          
-          
-        }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-      
+  render () {
+    console.log(this.props.items)
 
+    console.log(this.state.locationArray)
+    const { isLoaded, locationArray } = this.state
+    if (!isLoaded) {
+      return <div>Loading...</div>
+    } else {
+      return (
+        <div>
+          <Map
+            google={this.props.google}
+            zoom={14}
+            visible
+            initialCenter={{
+              lat: -41.286461,
+              lng: 174.776230
+            }}
+          >
+            {locationArray.map((marker, i) => (
+              <Marker
+                key={i}
+                name={'Name'}
+                position={{ lat: marker.lat, lng: marker.lng }}
+              />
+            ))}
+          </Map>
+        </div>
+      )
+    }
+  }
+
+  getLocations () {
+    console.log('running')
+
+    var locations = []
+    var count = 0
+    for (let i = 0; i < this.props.items.length; i++) {
+      var el = this.props.items[i]
+      // console.log(el);
+
+      Geocode.fromAddress(el['0'].address1 + ' Wellington, New Zealand').then(
+        response => {
+          count++
+          var lat = response.results[0].geometry.location.lat
+          var lng = response.results[0].geometry.location.lng
+
+          locations.push({
+            title: el[0].company,
+            lat: lat,
+            lng: lng
+          })
+
+          if (count === this.props.items.length) {
+            this.setState({
+              locationArray: locations,
+              isLoaded: true
+            })
+          }
+        },
+        error => {
+          console.log(error)
+        }
+      )
+    }
+  }
 }
 
 export default GoogleApiWrapper({
-  apiKey: (key[0].API_KEY)
+  apiKey: key[0].API_KEY
 })(MapContainer)
